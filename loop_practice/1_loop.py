3be9ff794a1dac226b1134ae4672cc76d2d63dd8
Tushar1-sati <kmukesh24757@gmail.com>
2026-02-23 23:23:51 +0530

    send

--- loop_practice/1_loop.py
+++ loop_practice/1_loop.py
@@ -15,6 +15,14 @@ for i in s1:
     if i in vowel:
         count=count+1
 print(count)
+# learning is int and string can't add understand the function.
 
 
 
+# Print numbers from 1 to 50 divisible by both 4 and 6.
+num=[]
+for i in range(1,50):
+    if i%4==0 and i%6==0:
+        num.append(i)
+print(num)
+# learning is we can't add integer in list for adding something in list use append function
