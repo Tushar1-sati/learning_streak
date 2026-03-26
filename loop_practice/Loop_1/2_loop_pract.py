1ef80d12887f705d051e5f22ee03975d3d37653b
Tushar1-sati <kmukesh24757@gmail.com>
2026-03-26 22:10:49 +0530

    concept

---
+++ loop_practice/Loop_1/2_loop_pract.py
@@ -0,0 +1,15 @@
+
+# multiplication
+n=int(input('enter the value for multiplication= '))
+for i in range(1,11):
+   c=i*n
+   print
+
+
+# You are given a string s, you need to print its characters at even indices(index starts at 0).
+
+s = "DoctorPhenomenal"
+for i in range(0,len(s),2):
+    print(s[i])
+
+   
